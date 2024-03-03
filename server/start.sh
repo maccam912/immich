1a5ec92fbb0c04706002af8eb4f02686bf931471
Matthew Koski <maccam912@gmail.com>
2024-03-02 19:23:11 -0600

    Update start.sh

--- server/start.sh
+++ server/start.sh
@@ -5,12 +5,15 @@ lib_path="/usr/lib/$(arch)-linux-gnu/libmimalloc.so.2"
 export LD_PRELOAD="$lib_path"
 
 read_file_and_export() {
-	if [ -n "${!1}" ]; then
-		content="$(cat "${!1}")"
-		export "$2"="${content}"
-		unset "$1"
-	fi
+    # First, let's check if the variable named by $1 is set.
+    if printenv | grep -q "^$1="; then
+        # Now we proceed with confidence that $1 names an existing env var.
+        content="$(cat "${!1}")"
+        export "$2"="${content}"
+        unset "$1"
+    fi
 }
+
 read_file_and_export "DB_URL_FILE" "DB_URL"
 read_file_and_export "DB_HOSTNAME_FILE" "DB_HOSTNAME"
 read_file_and_export "DB_DATABASE_NAME_FILE" "DB_DATABASE_NAME"
